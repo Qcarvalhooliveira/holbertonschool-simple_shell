a460775afd654c15c267c80d1412edf89287839d
SabyParis <sabrina-papeau@outlook.com>
2023-04-25 06:53:04 -0700

    get_path.c

--- get_path.c
+++ get_path.c
@@ -7,39 +7,37 @@
   * Return: command's path
   */
 
-char *get_path(char *getcmd) 
+char *get_path(char *getcmd)
 {
     char line[256];
-    char *path = malloc(256*sizeof(char));
-        
+    char *path = NULL;
+
     FILE *maps_file = fopen("/proc/self/maps", "r");
-    if (maps_file == NULL) 
-{
+    if (maps_file == NULL)
+    {
         fprintf(stderr, "Error: Impossible to open file/proc/self/maps.\n");
         exit(1);
-}
-    
-        
-    while (fgets(line, 256, maps_file) != NULL) 
+    }
 
-{
- 
-    char *start = strstr(line, "/");
-    char *end = strstr(line, getcmd);
-        
-    if (start != NULL && end != NULL) 
-    
-{
+    while (fgets(line, 256, maps_file) != NULL)
+    {
+        char *start = strstr(line, "/");
+        char *end = strstr(line, getcmd);
+
+        if (start != NULL && end != NULL)
+        {
+            path = malloc(strlen(start) + 1);
+            if (path == NULL)
+            {
+                fprintf(stderr, "Error: Memory allocation failed.\n");
+                exit(1);
+            }
             sscanf(start, "%s", path);
             break;
-}
-    
-
-}
-    
+        }
+    }
 
     fclose(maps_file);
-    
-    free(path); 
+
     return path;
 }
