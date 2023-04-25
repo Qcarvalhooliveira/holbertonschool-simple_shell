e7053d5fcf37b17bb29845df738e93f1c775cfd5
SabyParis <sabrina-papeau@outlook.com>
2023-04-25 05:54:27 -0700

    split.c

--- split.c
+++ split.c
@@ -1,23 +1,21 @@
 /**
   * _func_split- Function to make tokens (what is reading)
-  * @
+  * @line: the line to be tokenize
   * @
   *
-  * Return:
-  *
+  * Return: tokens's array
   */
 
-#include <strings.h>
+#include <stdlib.h>
+#include <string.h>
 
 char **func_split(char *line)
 
 {
 
 char **tokens = malloc(sizeof(char*));
-
-    
+ 
 char *token = strtok(line, " ");
-
    
 int i = 0;
 
@@ -26,20 +24,17 @@ while (token != NULL)
 {
         
 tokens[i] = token;
-
-        
+     
 i++;
 
 tokens = realloc(tokens, (i+1)*sizeof(char*));
 
- 
- token = strtok(NULL, " ");
+token = strtok(NULL, " ");
     
 }
 
     
 tokens[i] = NULL;
-
 
 return tokens;
 
