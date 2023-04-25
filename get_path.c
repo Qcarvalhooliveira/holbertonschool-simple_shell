f4be576d4fe93c6f8e608ee8ad8c7245706ce2aa
SabyParis <sabrina-papeau@outlook.com>
2023-04-24 22:41:45 -0700

    get_path.c

---
+++ get_path.c
@@ -0,0 +1,40 @@
+#include "shell.c"
+
+
+char *get_path(char *getcmd) 
+{
+    
+    char *path = malloc(256*sizeof(char));
+    
+    
+    FILE *maps_file = fopen("/proc/self/maps", "r");
+    if (maps_file == NULL) 
+{
+        fprintf(stderr, "Error: Impossible to open file/proc/self/maps.\n");
+        exit(1);
+}
+    
+    char line[256];
+    
+    while (fgets(line, 256, maps_file) != NULL) 
+
+{
+ 
+    char *start = strstr(line, "/");
+    char *end = strstr(line, getcmd);
+        
+    if (start != NULL && end != NULL) 
+    
+{
+            sscanf(start, "%s", path);
+            break;
+}
+    
+
+}
+    
+
+    fclose(maps_file);
+    
+    return path;
+}
