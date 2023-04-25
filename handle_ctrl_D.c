62bc012a240a6c91260d4a43618987c7fe5cc8bb
root <queisecarvalho@hotmail.com>
2023-04-25 04:36:00 -0700

    handle_ctrl_D

---
+++ handle_ctrl_D.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * handle_ctrl_d - Handles ctrl + d
+ * @buf: buffer to read input from
+ *
+ * Return: 1 if ctrl + d was pressed, otherwise 0
+ */
+
+int handle_ctrl_d(char *buf)
+{
+	ssize_t num_read;
+
+	num_read = read(STDIN_FILENO, buf, BUFSIZE);
+	if (num_read == 0)
+	{
+	write(STDOUT_FILENO, "exit\n", 5);
+	return (1);
+	}
+	return (0);
+}
