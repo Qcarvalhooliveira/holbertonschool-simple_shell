d93e88b3c7a2494f4ccb47008c408f246ad6decc
root <queisecarvalho@hotmail.com>
2023-04-26 14:19:08 -0700

    Shell.h

--- shell.h
+++ shell.h
@@ -12,8 +12,6 @@
 #include <dirent.h>
 #include <signal.h>
 
-
-/*constants*/
 #define EXTERNAL_COMMAND 1
 #define INTERNAL_COMMAND 2
 #define PATH_COMMAND 3
@@ -39,41 +37,29 @@ extern char *line;
 extern char **commands;
 extern char *shell_name;
 extern int status;
-
-/*helpers*/
 void print(char *, int);
 char **tokenizer(char *, char *);
 void remove_newline(char *);
 int _strlen(char *);
 void _strcpy(char *, char *);
-
-/*helpers2*/
 int _strcmp(char *, char *);
 char *_strcat(char *, char *);
 int _strspn(char *, char *);
 int _strcspn(char *, char *);
 char *_strchr(char *, char);
-
-/*helpers3*/
 char *_strtok_r(char *, char *, char **);
 int _atoi(char *);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void ctrl_c_handler(int);
 void remove_comment(char *);
-
-/*utils*/
 int parse_command(char *);
 void execute_command(char **, int);
 char *check_path(char *);
 void (*get_func(char *))(char **);
 char *_getenv(char *);
-
-/*built_in*/
 void env(char **);
 void quit(char **);
-
-/*main*/
 extern void non_interactive(void);
 extern void initializer(char **current_command, int type_command);
 
-#endif /*SHELL_H*/
+#endif
