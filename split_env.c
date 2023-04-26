b56661dcf5d51ac7097d7b906f998984492b6b2c
SabyParis <sabrina-papeau@outlook.com>
2023-04-25 23:27:37 -0700

    split_env.c

--- split_env.c
+++ split_env.c
@@ -11,19 +11,19 @@
 char **split_env(char *path)
 
 {
-    int i = 0;
-    char **tokens = malloc(sizeof(char*));
-    char *token = strtok(path, "/");
+	int i = 0;
+	char **tokens = malloc(sizeof(char *));
+	char *token = strtok(path, "/");
 
-    while (token != NULL) 
+	while (token != NULL)
 {
-        tokens[i] = token;
-        i++;
-        tokens = realloc(tokens, (i+1)*sizeof(char*));
-        token = strtok(NULL, "/");
+	tokens[i] = token;
+	i++;
+	tokens = realloc(tokens, (i + 1) * sizeof(char *));
+	token = strtok(NULL, "/");
 }
-    tokens[i] = NULL;
+	tokens[i] = NULL;
 
-    return (tokens);
+	return (tokens);
 }
 
