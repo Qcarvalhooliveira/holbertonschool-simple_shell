d741ccab6b4dec3b9410c6d45762056d80d8fc33
SabyParis <sabrina-papeau@outlook.com>
2023-04-25 23:26:55 -0700

    trim.c

--- trim.c
+++ trim.c
@@ -8,37 +8,36 @@
  */
 char *trim(char *str)
 {
-    char *front = str;
-    char *end = NULL;
-    int len = 0;
+	char *front = str;
+	char *end = NULL;
+	int len = 0;
 
-    if (str == NULL)
-        return (NULL);
-    if (str[0] == '\0')
-        return (str);
+	if (str == NULL)
+	return (NULL);
+	if (str[0] == '\0')
+	return (str);
 
-    len = strlen(str);
-    end = str + len;
+	len = strlen(str);
+	end = str + len;
 
-    while (*front == ' ')
-        front++;
+	while (*front == ' ')
+	front++;
 
-    if (end != front)
-    {
-        while (*end == ' ' && end != front)
-            end--;
-    }
-    if (front != str && end == front)
-        *str = '\0';
-    else if (str + len - 1 != end)
-        *(end + 1) = '\0';
-    end = str;
-    if (front != str)
-    {
-        while (*front)
-            *end++ = *front++;
-        *end = '\0';
-    }
-
-    return (str);
+	if (end != front)
+	{
+	while (*end == ' ' && end != front)
+	end--;
+	}
+	if (front != str && end == front)
+	*str = '\0';
+	else if (str + len - 1 != end)
+	*(end + 1) = '\0';
+	end = str;
+	if (front != str)
+	{
+	while (*front)
+	*end++ = *front++;
+	*end = '\0'
+	}
+	return (str);
 }
