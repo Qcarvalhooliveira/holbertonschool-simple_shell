0d7780165330e59a9dc8577e68cfbf066803ec6c
SabyParis <sabrina-papeau@outlook.com>
2023-04-26 12:32:46 -0700

    non_interactive_function.c shell.h shell_main.c shell_utils.c

--- shell.h
+++ shell.h
@@ -1,43 +1,79 @@
 #ifndef SHELL_H
 #define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
-#include <limits.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
 #include <signal.h>
 
-int main(int argc, char **argv, char **environ);
-char *_strcat(char *dest, char *src);
-void free_grid(char **grid);
-char *_strcat(char *dest, char *src);
-char *_strdup(char *str);
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *string);
-int _strncmp(char *string1, char *string2, int n);
-void free_grid(char **grid);
-int execute(char **parse);
-char *_getenv(char **environ, char *dirname);
-void _printenv(void);
-char *read_func(void);
-char **find_path(char **environ);
-char *args_path(char **parse, char **new);
-int handle_ctrl_d(char *buf);
-char **func_split(char *line);
-char **split_env(char *path);
-char **func_split(char *line);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char **splits(char *line, char *delim);
-char **split_env(char *path);
-char *length(char *str);
-int compare(char *varname, char *dirname);
-char *read_line(void);
-void _puts(char *str);
-unsigned int _strlen(char *s);
-char *if_exists(char **environ);
+
+/*constants*/
+#define EXTERNAL_COMMAND 1
+#define INTERNAL_COMMAND 2
+#define PATH_COMMAND 3
+#define INVALID_COMMAND -1
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
+/**
+ *struct map - a struct that maps a command name to a function 
+ *
+ *@command_name: name of the command
+ *@func: the function that executes the command
+ */
+
+typedef struct map
+{
+	char *command_name;
+	void (*func)(char **command);
+} function_map;
+
 extern char **environ;
-#endif
+extern char *line;
+extern char **commands;
+extern char *shell_name;
+extern int status;
+
+/*helpers*/
+void print(char *, int);
+char **tokenizer(char *, char *);
+void remove_newline(char *);
+int _strlen(char *);
+void _strcpy(char *, char *);
+
+/*helpers2*/
+int _strcmp(char *, char *);
+char *_strcat(char *, char *);
+int _strspn(char *, char *);
+int _strcspn(char *, char *);
+char *_strchr(char *, char);
+
+/*helpers3*/
+char *_strtok_r(char *, char *, char **);
+int _atoi(char *);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void ctrl_c_handler(int);
+void remove_comment(char *);
+
+/*utils*/
+int parse_command(char *);
+void execute_command(char **, int);
+char *check_path(char *);
+void (*get_func(char *))(char **);
+char *_getenv(char *);
+
+/*built_in*/
+void env(char **);
+void quit(char **);
+
+/*main*/
+extern void non_interactive(void);
+extern void initializer(char **current_command, int type_command);
+
+#endif /*SHELL_H*/
