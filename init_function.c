9901c41c73bda4135457a06a52f885d837557598
root <queisecarvalho@hotmail.com>
2023-04-27 04:55:45 -0700

    init_function

---
+++ init_function.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * init - Function that starts executing the program
+ * @current_command: check current token.
+ * @type_command: parse token
+ *
+ * Return: void function
+ */
+
+void init(char **current_command, int type_command)
+{
+	pid_t PID;
+
+	if (type_command == EXTERNAL_COMMAND || type_command == PATH_COMMAND)
+	{
+		PID = fork();
+		if (PID == 0)
+			execute_command(current_command, type_command);
+		else
+		{
+			waitpid(PID, &status, 0);
+			status >>= 8;
+		}
+	}
+	else
+		execute_command(current_command, type_command);
+}
